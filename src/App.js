161be0cf65f6004d4f27a9162c6dbcf34f593dea
Brian Ruff <brff19@gmail.com>
2018-03-17 00:34:24 -0400

    react weather app

---
+++ src/App.js
@@ -0,0 +1,91 @@
+import React from "react";
+import Titles from "./Components/Titles";
+import Form from "./Components/Form";
+import Weather from "./Components/Weather";
+
+const API_KEY = "b3cf48ace991ca96167230cd72414432";
+
+class App extends React.Component {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    windSpeed: undefined,
+    min_temp: undefined,
+    max_temp: undefined,
+    error: undefined
+  };
+
+  getWeather = async e => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`
+    );
+    const data = await api_call.json();
+
+    if (city && country) {
+      console.log(data);
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        windSpeed: data.wind.speed,
+        min_temp: data.main.temp_min,
+        max_temp: data.main.temp_max,
+        error: ""
+      });
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        windSpeed: undefined,
+        min_temp: undefined,
+        max_temp: undefined,
+        error: "Please Enter City and Country"
+      });
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="wrapper">
+          <div className="main">
+            <div className="container">
+              <div className="row">
+                <div className="col-md-5 title-container">
+                  <Titles />
+                </div>
+                <div className="col-md-7 form-container">
+                  <Form getWeather={this.getWeather} />
+                  <Weather
+                    temperature={this.state.temperature}
+                    city={this.state.city}
+                    country={this.state.country}
+                    humidity={this.state.humidity}
+                    description={this.state.description}
+                    windSpeed={this.state.windSpeed}
+                    min_temp={this.state.min_temp}
+                    max_temp={this.state.max_temp}
+                    error={this.state.error}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Components/Weather.js
@@ -0,0 +1,62 @@
+import React from "react";
+
+const Weather = props => (
+  <div className="weather__info">
+    {props.city &&
+      props.country && (
+        <p className='weather__key'>
+          Location:
+          <span className='weather__value'> {props.city}, {props.country} </span>
+        </p>
+      )}
+
+    {props.temperature && (
+      <p className='weather__key'>
+        Temperature: 
+        <span className='weather__value'> {Math.round(9 / 5 * (props.temperature - 273) + 32)}&deg;F</span>
+      </p>
+    )}
+
+    {props.min_temp && (
+      <p className='weather__key'>
+        Minimum Temp:
+        <span className='weather__value'> {Math.round(9 / 5 * (props.min_temp - 273) + 32)}&deg;F}</span>
+      </p>
+    )}
+
+    {props.max_temp && (
+      <p className='weather__key'>
+        Maximum Temp:
+        <span className='weather__value'> {Math.round(9 / 5 * (props.max_temp - 273) + 32)}&deg;F </span>
+      </p>
+    )}
+
+    {props.humidity && (
+      <p className='weather__key'>
+        Humidity:
+        <span className='weather__value'> {props.humidity}</span>
+      </p>
+    )}
+
+    {props.description && (
+      <p className='weather__key'>
+        Condition:
+        <span className='weather__value'> {props.description}</span>
+      </p>
+    )}
+
+    {props.windSpeed && (<p className='weather__key'>
+        Wind Speed:<span className='weather__value'> {Math.round(props.windSpeed.toFixed(1))} mph</span>
+      </p>
+    )}
+
+    {props.error && 
+    <p className='weather__error'>
+        {props.error}
+    </p>
+    }
+
+  </div>
+);
+
+export default Weather;
