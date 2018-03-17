738a7fd7bf73307fd066c4d689dad2ccfd9a638f
Brian Ruff <brff19@gmail.com>
2018-03-17 14:56:22 -0400

    added C and K

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -13,21 +13,21 @@ const Weather = props => (
     {props.temperature && (
       <p className='weather__key'>
         Temperature: 
-        <span className='weather__value'> {Math.round(9 / 5 * (props.temperature - 273) + 32)}&deg;F</span>
+        <span className='weather__value'> {Math.round(9 / 5 * (props.temperature - 273) + 32)}&deg;F, {(props.temperature - 273.15).toFixed(2)}&deg;C, {props.temperature.toFixed(2)}&deg;K</span>
       </p>
     )}
 
     {props.min_temp && (
       <p className='weather__key'>
         Minimum Temp:
-        <span className='weather__value'> {Math.round(9 / 5 * (props.min_temp - 273) + 32)}&deg;F</span>
+        <span className='weather__value'> {Math.round(9 / 5 * (props.min_temp - 273) + 32)}&deg;F, {(props.min_temp - 273.15).toFixed(2)}&deg;C, {props.min_temp.toFixed(2)}&deg;K</span>
       </p>
     )}
 
     {props.max_temp && (
       <p className='weather__key'>
         Maximum Temp:
-        <span className='weather__value'> {Math.round(9 / 5 * (props.max_temp - 273) + 32)}&deg;F </span>
+        <span className='weather__value'> {Math.round(9 / 5 * (props.max_temp - 273) + 32)}&deg;F, {(props.max_temp - 273.15).toFixed(2)}&deg;C, {props.max_temp.toFixed(2)}&deg;K </span>
       </p>
     )}
 
