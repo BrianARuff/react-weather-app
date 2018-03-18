b0b4b0c481f4e5a20373ce9d1ff99ad2ec91b75b
Brian Ruff <brff19@gmail.com>
2018-03-18 19:19:18 -0400

    changed name of app in manifest file

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -13,21 +13,21 @@ const Weather = props => (
     {props.temperature && (
       <p className='weather__key'>
         Temperature: 
-        <span className='weather__value'> {Math.round(9 / 5 * (props.temperature - 273) + 32)}&deg;F, {(props.temperature - 273.15).toFixed(2)}&deg;C, {props.temperature.toFixed(2)}&deg;K</span>
+        <span className='weather__value'> {Math.round(9 / 5 * (props.temperature - 273) + 32)}&deg;F, {(props.temperature - 273.15).toFixed(2)}&deg;C</span>
       </p>
     )}
 
     {props.min_temp && (
       <p className='weather__key'>
         Minimum Temp:
-        <span className='weather__value'> {Math.round(9 / 5 * (props.min_temp - 273) + 32)}&deg;F, {(props.min_temp - 273.15).toFixed(2)}&deg;C, {props.min_temp.toFixed(2)}&deg;K</span>
+        <span className='weather__value'> {Math.round(9 / 5 * (props.min_temp - 273) + 32)}&deg;F, {(props.min_temp - 273.15).toFixed(2)}&deg;C</span>
       </p>
     )}
 
     {props.max_temp && (
       <p className='weather__key'>
         Maximum Temp:
-        <span className='weather__value'> {Math.round(9 / 5 * (props.max_temp - 273) + 32)}&deg;F, {(props.max_temp - 273.15).toFixed(2)}&deg;C, {props.max_temp.toFixed(2)}&deg;K </span>
+        <span className='weather__value'> {Math.round(9 / 5 * (props.max_temp - 273) + 32)}&deg;F, {(props.max_temp - 273.15).toFixed(2)}&deg;C</span>
       </p>
     )}
 
@@ -45,7 +45,8 @@ const Weather = props => (
       </p>
     )}
 
-    {props.windSpeed && (<p className='weather__key'>
+    {props.windSpeed && (
+      <p className='weather__key'>
         Wind Speed:<span className='weather__value'> {Math.round(props.windSpeed.toFixed(1))} mph</span>
       </p>
     )}
